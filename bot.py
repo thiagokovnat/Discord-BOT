d0aa4552521312b5dc131652d8e7321074ad1d6d
Thiago Kovnat <49885922+thiagokovnat@users.noreply.github.com>
2020-01-19 02:08:19 -0300

    Added Commands

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import hashlib
 import random
 from discord.ext import commands
 
+
+
 client = commands.Bot(command_prefix="!")
-token = YOUR-TOKEN
-GUILD = YOUR-SERVER-ID
+token = "NjY3OTA5NTkxNjE4NTUxODI5.XiNkgg.X8rKE5-83fH5KxwB7rxeq_x2fjA"
+GUILD = 668246451863814155
 joined = 0
 messages = 0
 
@@ -18,7 +20,7 @@ async def on_ready():
         if guild == GUILD:
             break
 
-        print(f"""Ah yes, {client.user} is now connected to Discord on Guild {guild.name}, {guild.id}!""")
+        print(f"""{client.user} is now connected to Discord on Guild {guild.name}, {guild.id}!""")
 
 
 @client.event
@@ -120,20 +122,59 @@ async def md5(ctx, *args):
     await ctx.send(encryptedRes.hexdigest())
 
 
-@client.command(name = "callouts", help = "gives a link to image of CSGO Callouts")
+@client.command(name = "callouts", help = "shows an image of CSGO Callouts")
 async def giveURL(ctx, map):
 
+    Embedded = discord.Embed()
     if map.lower() == "mirage":
-        await ctx.send("Mirage: https://steamuserimages-a.akamaihd.net/ugc/771726758015167515/920A3B2823E12A734E4BBED38FC518FE6626D9BE/")
+        Embedded.set_image(url = "https://steamuserimages-a.akamaihd.net/ugc/771726758015167515/920A3B2823E12A734E4BBED38FC518FE6626D9BE/")
+        Embedded.set_footer(text = "Mirage")
+        await ctx.send(content = None, embed = Embedded)
     elif map.lower() == "inferno":
-        await ctx.send("Inferno: https://steamuserimages-a.akamaihd.net/ugc/763846954789928908/456494D0616C3DD65431B8E5E4C843E92FF08114/")
+        Embedded.set_image(url = "https://steamuserimages-a.akamaihd.net/ugc/763846954789928908/456494D0616C3DD65431B8E5E4C843E92FF08114/")
+        Embedded.set_footer(text = "Inferno")
+        await ctx.send(content=None, embed = Embedded)
     elif map.lower() == "cache":
-        await ctx.send("Cache: https://steamuserimages-a.akamaihd.net/ugc/788604629983432599/D6B07179997628242DF87282F063B4B0454839A1/")
+        Embedded.set_image(url = " https://steamuserimages-a.akamaihd.net/ugc/788604629983432599/D6B07179997628242DF87282F063B4B0454839A1/")
+        Embedded.set_footer(text = "Cache")
+        await ctx.send(content=None, embed = Embedded)
     elif map.lower() == "dust":
-        await ctx.send("Dust 2: https://steamuserimages-a.akamaihd.net/ugc/849347535114322925/AE8CE3B6FF828B3EF09779408A13788ED3BC99A6/?imw=512&imh=513&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")
+        Embedded.set_image(url="https://steamuserimages-a.akamaihd.net/ugc/849347535114322925/AE8CE3B6FF828B3EF09779408A13788ED3BC99A6/?imw=512&imh=513&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")
+        Embedded.set_footer(text="Dust 2")
+        await ctx.send(content=None, embed = Embedded)
     elif map.lower() == "list":
-        await ctx.send("Current Maps: Mirage, Inferno, Cache, Dust")
+        Embedded.add_field(name = "Mirage", value = "!callouts mirage", inline = False)
+        Embedded.add_field(name = "Inferno", value = "!callouts inferno", inline = False)
+        Embedded.add_field(name = "Cache", value = "!callouts cache", inline = False)
+        Embedded.add_field(name = "Dust 2", value = "!callouts dust", inline = False)
+        await ctx.send(content = None, embed = Embedded)
 
+#@client.command(name = "stats", help = "gives your current CSGO Stats")
+#async def getStats(ctx, steamId):
+    #url = "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=12A1D1DE83F9932934EDD6DF2BA00463&steamid=" + str(steamId)
+    #r = requests.get(url).json()
+
+    #stats = {
+       # 'total_kills': r['playerstats']['stats'][0]['value'],  # total kills
+      #  'total_deaths': r['playerstats']['stats'][1]['value'],  # total deaths
+     #   'total_time': r['playerstats']['stats'][2]['value']  # total time played
+    #}
+
+   # await ctx.send(f"""Total Kills: {stats["total_kills"]}, Total Deaths: {stats['total_deaths']}, Time Played: {stats['total_time']/3600}h. KD: {stats["total_kills"]/stats['total_deaths']}""")
+
+@client.command(name = "ping", help = "returns current bot ping")
+async def ping(ctx):
+
+    await ctx.send("Current bot ping: " + str(client.latency) + "ms")
+
+@client.command(name = "ban", help = "Bans player from server")
+async def ban(ctx, User : discord.Member, reason = None):
+    await User.ban(reason = reason)
+
+@client.command(name = "clear", help = "deletes x messages")
+@commands.has_permissions(manage_messages = True)
+async def clear(ctx, amount = 10):
+    await ctx.channel.purge(limit = amount)
 
 async def update_stats():
     await client.wait_until_ready()
